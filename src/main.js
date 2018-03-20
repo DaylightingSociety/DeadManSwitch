0e542819375e5fabb408b47e1d72f7a3e3983058
Ethan Riley <ethanmriley@gmail.com>
2018-03-20 18:10:11 -0400

    timeout implemented

--- src/main.js
+++ src/main.js
@@ -1,37 +1,39 @@
 const nodemailer = require('nodemailer');
+const waitMS = 5000;
 
-nodemailer.createTestAccount((err, account) => {
+console.log('Credentials obtained, sending message...');
+
+let transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email', 
+    port: 587,
+    auth: {
+        user: 'ympjsencdzuybpy5@ethereal.email',
+        pass: 'feDttypBmBHBFshJdg'
+    }
+});
+
+let message = {
+    from: 'dms test <dms_test@fake.domain>',
+    to: 'fake recipient <fake_recipient@fake.domain>', 
+    subject: 'DMS Test', 
+    text: 'Testing the DMS!',
+    html: 'Testing the <strong>DMS!</strong>'
+};
+
+var sendMailCallback = function(err, info) {
     if (err) {
-        console.error('Failed to create a testing account. ' + err.message);
+        console.log('Error occured when sending. ' + error.message);
         return process.exit(1);
     }
 
-    console.log('Credentials obtained, sending message...');
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+};
 
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email', 
-        port: 587,
-        auth: {
-            user: 'ympjsencdzuybpy5@ethereal.email',
-            pass: 'feDttypBmBHBFshJdg'
-        }
-    });
+var sendMail = function() {
+    transporter.sendMail(message, sendMailCallback);
+}
 
-    let message = {
-        from: 'dms test <dms_test@fake.domain>',
-        to: 'fake recipient <fake_recipient@fake.domain>', 
-        subject: 'DMS Test', 
-        text: 'Testing the DMS!',
-        html: 'Testing the <strong>DMS!</strong>'
-    };
+console.log('waiting for %d ms', waitMS);
 
-    transporter.sendMail(message, (err, info) => {
-        if (err) {
-            console.log('Error occured when sending. ' + error.message);
-            return process.exit(1);
-        }
-
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    });
-});
+setTimeout(sendMail, waitMS);
