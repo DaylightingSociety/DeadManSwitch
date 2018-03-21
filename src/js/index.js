e593742dea29969f2d05d859a72be6063c3fc4bb
Ethan Riley <ethanmriley@gmail.com>
2018-03-21 10:31:19 -0400

    sendDate now sent via ipc from renderer to main

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,17 @@
 const {ipcRenderer} = require('electron');
-const sendDate = new Date(2018, 3, 21);
-const task = {
-    sendDate: sendDate, 
-    from: 'test', 
-    to: 'test',
-    subject: 'hey', 
-    text: 'what\'s up'
-};
 
-ipcRenderer.send('new_task', task);
+var dateField = document.body.querySelector('form input[type="datetime-local"]');
+var submit = document.body.querySelector('button');
 
-console.log('sent!')
+submit.addEventListener("click", () => {
+    const sendDate = new Date(dateField.value);
+    const task = {
+        sendDate: sendDate, 
+        from: 'test', 
+        to: 'test',
+        subject: 'hey', 
+        text: 'what\'s up'
+    };
+    
+    ipcRenderer.send('new_task', task);
+});
--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,11 @@ const {ipcMain, app, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
 
-const sendDate = new Date(2018, 2, 21, 23, 0, 0, 0);
-const waitMS = sendDate.getTime() - Date.now();
-const waitTime = new Date(waitMS);
+var sendDate;
+var waitMS;
+var waitTime;
 
-if (waitMS < 0) {
-    //do something
-}
-
-let transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
     host: 'smtp.ethereal.email', 
     port: 587,
     auth: {
@@ -20,7 +16,7 @@ let transporter = nodemailer.createTransport({
     }
 });
 
-let message = {
+var message = {
     from: 'dms test <dms_test@fake.domain>',
     to: 'fake recipient <fake_recipient@fake.domain>', 
     subject: 'DMS Test', 
@@ -28,7 +24,7 @@ let message = {
     html: 'Testing the <strong>DMS!</strong>'
 };
 
-let sendMailCallback = function(err, info) {
+function sendMailCallback(err, info) {
     if (err) {
         console.log('Error occured when sending. ' + error.message);
         return process.exit(1);
@@ -39,19 +35,9 @@ let sendMailCallback = function(err, info) {
     return;
 };
 
-let sendMail = function() {
+function sendMail() {
     transporter.sendMail(message, sendMailCallback);
 }
-
-console.log('waiting for %d minutes and %d seconds', waitTime.getMinutes(), waitTime.getSeconds()); 
-
-setTimeout(sendMail, waitMS); //this is async
-
-ipcMain.on('new_task', (event, arg) => {
-    console.log('hello!');
-    console.log(arg);
-});
-
 
 function createWindow() {
     win = new BrowserWindow({width: 600, height: 800});
@@ -63,3 +49,13 @@ function createWindow() {
 }
 
 app.on('ready', createWindow);
+
+ipcMain.on('new_task', (event, arg) => {
+    sendDate = new Date(arg.sendDate);
+    waitMS = sendDate.getTime() - Date.now();
+    waitTime = new Date(waitMS);
+
+    console.log('sending in %d days, %d minutes, and %d seconds', waitTime.getDate(), waitTime.getMinutes(), waitTime.getSeconds());
+
+    setTimeout(sendMail, waitMS); //this is async
+});
