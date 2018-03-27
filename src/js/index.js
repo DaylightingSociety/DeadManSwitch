a7ac09ebcc9a3f2b94161ac5f29dc4d52ae5bf44
Ethan Riley <ethanmriley@gmail.com>
2018-03-27 15:42:07 -0400

    added support for mail.com

--- src/js/index.js
+++ src/js/index.js
@@ -37,32 +37,42 @@ config.addEventListener("change", function() {
 });
 
 const gmailConfig = {
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: '',
-            pass: ''
-        }
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: '',
+        pass: ''
+    }
 };
 
 const etherealConfig = {
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-            user: 'ympjsencdzuybpy5@ethereal.email',
-            pass: 'feDttypBmBHBFshJdg'
-        }
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'ympjsencdzuybpy5@ethereal.email',
+        pass: 'feDttypBmBHBFshJdg'
+    }
 };
 
 const riseupConfig = {
-        host: 'mail.riseup.net',
-        port: 465,
-        secure: true,
-        auth: {
-            user: '',
-            pass: ''
-        }
+    host: 'mail.riseup.net',
+    port: 465,
+    secure: true,
+    auth: {
+        user: '',
+        pass: ''
+    }
+};
+
+const mailConfig = {
+    host: 'smtp.mail.com', 
+    port: 465,
+    securee: true, 
+    auth: {
+        user: '', 
+        pass: ''
+    }
 };
 
 submit.addEventListener("click", function() {
@@ -90,8 +100,24 @@ submit.addEventListener("click", function() {
             task.config.auth.user = username.value;
             task.config.auth.pass = password.value;        
             break;
+        case 'mail':
+            task.config = mailConfig;
+            task.config.auth.user = username.value;
+            task.config.auth.pass = password.value;
+            break;
         case 'ethereal': //since ethereal is for testing, we won't need to supply creds
             task.config = etherealConfig;
+            break;
+        case 'custom':
+            task.config = {
+                host: domain.value,
+                port: port.value,
+                secure: secure.checked,
+                auth: {
+                    user: username.value, 
+                    pass: password.value,
+                }
+            };
             break;
         default:
             task.config = etherealConfig;
