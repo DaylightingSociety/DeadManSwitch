152cacb28e49ba5e75478e19da753c49be7111f5
Ethan Riley <ethanmriley@gmail.com>
2018-04-04 18:12:49 -0400

    fixed some stuff
    fixes #13
    
    fixes #12

--- src/js/index.js
+++ src/js/index.js
@@ -4,39 +4,15 @@ const {ipcRenderer} = require('electron');
 window.$ = window.jQuery = require('jquery');
 window.bootstrap = require('bootstrap');
 
-var from = document.getElementById('input[id="from"]');
-var to = document.getElementById('input[id="to"');
-var subject = document.getElementById('input[name="subject"]');
-var text   = document.getElementById('input[name="text"]');
-var submit = document.getElementById('button[id="submit"]');
-var username = document.getElementById('input[name="username"]');
-var password = document.getElementById('input[name="password"]');
-var domain = document.getElementById('input[name="domain"]');
-var port = document.getElementById('input[name="port"]');
-var secure = document.getElementById('input[id="secure"]');
-var secureLabel = document.getElementById('label');
-var dateField = document.getElementById('input[type="datetime-local"]');
-
-
 $('#provider').on("change", function() {
-    if(config.value === "ethereal") {
-        username.setAttribute('type', 'hidden');
-        password.setAttribute('type', 'hidden');
+    if($('#provider').val() === "custom") {
+        $('#domain-wrapper').show();
+        $('#port-wrapper').show();
+        $('#secure-wrapper').show()
     } else {
-        username.setAttribute('type', 'text');
-        password.setAttribute('type', 'text');
-    }
-
-    if(config.value === "custom") {
-        domain.setAttribute('type', 'text');
-        port.setAttribute('type', 'text');
-        secure.setAttribute('type', 'checkbox');
-        secureLabel.setAttribute('style', 'display: inline;');
-    } else {
-        domain.setAttribute('type', 'hidden');
-        port.setAttribute('type', 'hidden');
-        secure.setAttribute('type', 'hidden');
-        secureLabel.setAttribute('style', 'display: none;');
+        $('#domain-wrapper').hide();
+        $('#port-wrapper').hide();
+        $('#secure-wrapper').hide();
     }
 });
 
@@ -80,52 +56,46 @@ const mailConfig = {
 };
 
 $('#submit').on("click", function() {
-    const sendDate = new Date(dateField.value);
+    const sendDate = new Date($('#date').val());
 
     const task = { //is this secure?
         sendDate: sendDate, 
           config: {}, 
          message: {
-              from: from.value, 
-                to: to.value,
-           subject: subject.value, 
-              text: text.value
+              from: $('#from').text(), 
+              to: $('#to').text(), 
+              subject: $('#subject').text(), 
+              text: $('#text').text(), 
         }
     };
     
+    let user = $('#username').text();
+    let pass = $('#password').text();
+
     switch(config.value) {
         case 'riseup':
             task.config = riseupConfig;
-            task.config.auth.user = username.value;
-            task.config.auth.pass = password.value;        
             break;
         case 'gmail':
             task.config = gmailConfig;
-            task.config.auth.user = username.value;
-            task.config.auth.pass = password.value;        
             break;
         case 'mail':
             task.config = mailConfig;
-            task.config.auth.user = username.value;
-            task.config.auth.pass = password.value;
-            break;
-        case 'ethereal': //since ethereal is for testing, we won't need to supply creds
-            task.config = etherealConfig;
             break;
         case 'custom':
             task.config = {
-                host: domain.value,
-                port: port.value,
-                secure: secure.checked,
-                auth: {
-                    user: username.value, 
-                    pass: password.value,
-                }
+                host: $('#domain').text(),
+                port: $('#port').text(),
+                secure: $('#secure').prop("checked"),
+                auth: {}
             };
             break;
-        default:
-            task.config = etherealConfig;
     }
+    
+    task.config.auth.user = user;
+    task.config.auth.pass = pass;
+
+    console.log(task);
     
     ipcRenderer.send('new_task', task);
 });
